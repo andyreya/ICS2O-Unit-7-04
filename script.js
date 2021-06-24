73df7297ec696410dfc7b74bdf728c76ff0845b3
andyreya <83035508+andyreya@users.noreply.github.com>
2021-06-24 00:07:22 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+//makes the function occur when the button is clicked
+document.getElementById('button').addEventListener('click', check)
+//creates variables
+let age = 0
+let date = 'Saturday'
+//creates a function that will
+function check () {
+  age = document.getElementById('input1').value
+  age = parseInt(age)
+
+  date = document.getElementById('input2').value
+
+  if ((date === 'Wednesday' || date === 'Friday') || (age > 12 && age < 21)) {
+    document.getElementById('answer').innerHTML = 'You get student pricing.'
+  } else {
+    document.getElementById('answer').innerHTML = 'You have to pay regular pricing.'
+  }
+}
